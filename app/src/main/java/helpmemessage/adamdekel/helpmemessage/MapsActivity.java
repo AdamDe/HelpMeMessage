3da962b5cc644e3361c894d7e719b7cfabee7ca5
AdamDe <adamdekel@gmail.com>
2018-08-27 13:01:12 +0300

    Initial commit

---
+++ app/src/main/java/helpmemessage/adamdekel/helpmemessage/MapsActivity.java
@@ -0,0 +1,58 @@
+package helpmemessage.adamdekel.helpmemessage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+import helpmemessage.adamdekel.helpmemessage.R;
+
+/**
+ * A styled map using JSON styles from a string resource.
+ */
+public class MapsActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private static final String TAG = MapsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_maps);
+
+        // Get the SupportMapFragment and register for the callback
+        // when the map is ready for use.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready for use.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        // Customise the styling of the base map using a JSON object defined
+        // in a string resource file. First create a MapStyleOptions object
+        // from the JSON styles string, then pass this to the setMapStyle
+        // method of the GoogleMap object.
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.map_style_json)));
+
+        if (!success) {
+            Log.e(TAG, "Style parsing failed.");
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+    }
+}
